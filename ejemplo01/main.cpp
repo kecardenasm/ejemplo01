999b265fe730b4ca232b447cd9ca7fd45177d338
Kevin Eduardo Cardenas mamani <kevin.kc541@gmail.com>
2021-09-10 10:04:31 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+		std::cout << "Nombres: Kevin Eduardo" << std::endl;
+		std::cout << "Apellidos: Cardenas Mamani" << std::endl;
+		std::cout << "Edad: 21" << std::endl;
+		std::cout << "Parientes: Jaime, alias el Jhimmy Cardenas" << std::endl;
+		std::cout << "Estado Civil: Soltero" << std::endl;
+		std::cout << "Sexo: Masculino" << std::endl;
+		std::cout << "Antecedentes: No" << std::endl;
+	return 0;
+}
